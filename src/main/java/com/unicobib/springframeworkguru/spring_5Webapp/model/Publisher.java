efc8cf6357d297a6e10444614547fef1b8e7503c
Bibek Dey <33726347+unicobib@users.noreply.github.com>
2019-07-20 02:52:18 +0530

    Add model,Repository for publisher and html pages

---
+++ src/main/java/com/unicobib/springframeworkguru/spring_5Webapp/model/Publisher.java
@@ -0,0 +1,43 @@
+package com.unicobib.springframeworkguru.spring_5Webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Publisher(){}
+
+    public Publisher(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Publisher)) return false;
+        Publisher publisher = (Publisher) o;
+        return id.equals(publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
