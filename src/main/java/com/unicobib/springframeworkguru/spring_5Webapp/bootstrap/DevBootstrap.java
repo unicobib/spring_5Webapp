01a04babe00636526f345f6fb256019fe671b381
Bibek Dey <33726347+unicobib@users.noreply.github.com>
2019-07-20 01:48:40 +0530

    Add book controller Add thymeleaf template

--- src/main/java/com/unicobib/springframeworkguru/spring_5Webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/unicobib/springframeworkguru/spring_5Webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.unicobib.springframeworkguru.spring_5Webapp.bootstrap;
 
 import com.unicobib.springframeworkguru.spring_5Webapp.model.Author;
 import com.unicobib.springframeworkguru.spring_5Webapp.model.Book;
+import com.unicobib.springframeworkguru.spring_5Webapp.model.Publisher;
 import com.unicobib.springframeworkguru.spring_5Webapp.repository.AuthorRepository;
 import com.unicobib.springframeworkguru.spring_5Webapp.repository.BookRepository;
+import com.unicobib.springframeworkguru.spring_5Webapp.repository.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     public AuthorRepository authorRepo;
     public BookRepository bookRepo;
+    public PublisherRepository publisherRepo;
 
-    public DevBootstrap(AuthorRepository authorRepo, BookRepository bookRepo) {
+    public DevBootstrap(AuthorRepository authorRepo, BookRepository bookRepo,PublisherRepository publisherRepo) {
         this.authorRepo = authorRepo;
         this.bookRepo = bookRepo;
+        this.publisherRepo = publisherRepo;
     }
 
     @Override
@@ -27,19 +31,24 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private void initData(){
 
         Author bibek = new Author("Bibek","Dey");
-        Book aaa = new Book("Java Design Patterns","1234","oxford");
+        Publisher pub1 = new Publisher("oxford");
+        Book aaa = new Book("Java Design Patterns","1234",pub1);
         bibek.getBooks().add(aaa);
         aaa.getAuthors().add(bibek);
 
         Author mahesh = new Author("Maheshwar","Garai");
-        Book bbb = new Book("Microservices Architecture","3659","Packt Pub");
+        Publisher pub2 = new Publisher("Packt Pub");
+        Book bbb = new Book("Microservices Architecture","3659",pub2);
         mahesh.getBooks().add(bbb);
         bbb.getAuthors().add(mahesh);
 
         authorRepo.save(bibek);
+        publisherRepo.save(pub1);
         bookRepo.save(aaa);
 
+
         authorRepo.save(mahesh);
+        publisherRepo.save(pub2);
         bookRepo.save(bbb);
     }
 }
